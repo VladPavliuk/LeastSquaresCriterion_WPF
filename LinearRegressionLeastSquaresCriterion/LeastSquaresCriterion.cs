fcbc7ea8863258d66f1e6889520b60bece18bd32
vlad <pavliuk.vlad@gmail.com>
2018-12-04 23:27:17 +0200

    Add implementation of linear regression least squares criterion algorithms.

---
+++ LinearRegressionLeastSquaresCriterion/LeastSquaresCriterion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearRegressionLeastSquaresCriterion
+{
+    public static class LeastSquaresCriterion
+    {
+        public static (double, double) Execute(List<(double, double)> inputData)
+        {
+            var slope = CalculateSlope(inputData);
+            var intercept = CalculateIntercept(inputData, slope);
+
+            return (slope, intercept);
+        }
+
+        private static double CalculateSlope(List<(double, double)> inputData)
+        {
+            double sumOfX = 0;
+            double sumOfY = 0;
+            double sumOfXY = 0;
+            double sumOfXSquare = 0;
+
+            foreach (var item in inputData)
+            {
+                sumOfX += item.Item1;
+                sumOfXSquare += Math.Pow(item.Item1, 2);
+                sumOfY += item.Item2;
+                sumOfXY += item.Item1 * item.Item2;
+            }
+
+            return (sumOfXY - sumOfX * sumOfY / inputData.Count) / (sumOfXSquare - Math.Pow(sumOfX, 2) / inputData.Count);
+        }
+
+        private static double CalculateIntercept(List<(double, double)> inputData, double slope)
+        {
+            double sumOfX = 0;
+            double sumOfY = 0;
+
+            foreach (var item in inputData)
+            {
+                sumOfX += item.Item1;
+                sumOfY += item.Item2;
+            }
+
+            return (sumOfY - slope * sumOfX) / inputData.Count;
+        }
+    }
+}
--- LinearRegressionLeastSquaresCriterion/MainWindow.xaml.cs
+++ LinearRegressionLeastSquaresCriterion/MainWindow.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace LinearRegressionLeastSquaresCriterion
@@ -20,9 +11,106 @@ namespace LinearRegressionLeastSquaresCriterion
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int oneUnitLength = 12;
+
+        public List<(double, double)> inputData = new List<(double, double)>
+            {
+                (1, 1),
+                (2, 2),
+                (3, 1.3),
+                (4, 3.75),
+                (5, 2.25)
+            };
+
         public MainWindow()
         {
             InitializeComponent();
+            var regresionLine = LeastSquaresCriterion.Execute(inputData);
+
+            DrawNet();
+
+            DrawLinearGragh(regresionLine.Item1, regresionLine.Item2);
+
+            GraphLable.Content = $"y = {Math.Round(regresionLine.Item1, 4)} x" +
+                (regresionLine.Item2 < 0 ? " - " : " + ") +
+                $"{Math.Abs(Math.Round(regresionLine.Item2, 4))}";
+        }
+
+        private void DrawLinearGragh(double slope, double intercept)
+        {
+            var rangeOfX = (-10_000, 10_000);
+
+            DrawLine(
+                (rangeOfX.Item1, (int)(slope * rangeOfX.Item1 + intercept)),
+                (rangeOfX.Item2, (int)(slope * rangeOfX.Item2 + intercept))
+            );
+        }
+
+        private void DrawNet()
+        {
+            var canvasWidth = GraphPlain.Width;
+            var canvasHeight = GraphPlain.Height;
+
+            for (var i = 0; i < canvasWidth; i++)
+            {
+                GraphPlain.Children.Add(
+                    new Line()
+                    {
+                        Stroke = Brushes.LightSteelBlue,
+                        X1 = i * oneUnitLength,
+                        Y1 = 0,
+                        X2 = i * oneUnitLength,
+                        Y2 = canvasHeight
+                    });
+            }
+
+            for (var j = 0; j < canvasHeight; j++)
+            {
+                GraphPlain.Children.Add(new Line()
+                {
+                    Stroke = Brushes.LightSteelBlue,
+                    X1 = 0,
+                    Y1 = j * oneUnitLength,
+                    X2 = canvasWidth,
+                    Y2 = j * oneUnitLength
+                });
+            }
+
+            GraphPlain.Children.Add(new Line()
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 3,
+                X1 = canvasWidth / 2,
+                Y1 = 0,
+                X2 = canvasWidth / 2,
+                Y2 = canvasHeight
+            });
+
+            GraphPlain.Children.Add(new Line()
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 3,
+                X1 = 0,
+                Y1 = canvasHeight / 2,
+                X2 = canvasWidth,
+                Y2 = canvasHeight / 2
+            });
+        }
+
+        private void DrawLine((int, int) firstPoint, (int, int) secondPoint)
+        {
+            var canvasWidth = GraphPlain.Width;
+            var canvasHeight = GraphPlain.Height;
+
+            Line line = new Line();
+
+            line.Stroke = SystemColors.WindowFrameBrush;
+            line.X1 = canvasWidth / 2 + firstPoint.Item1 * oneUnitLength;
+            line.Y1 = canvasHeight / 2 - firstPoint.Item2 * oneUnitLength;
+            line.X2 = canvasWidth / 2 + secondPoint.Item1 * oneUnitLength;
+            line.Y2 = canvasHeight / 2 - secondPoint.Item2 * oneUnitLength;
+
+            GraphPlain.Children.Add(line);
         }
     }
 }
